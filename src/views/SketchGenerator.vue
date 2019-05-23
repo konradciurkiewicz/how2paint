acc0044a4c2810f1600a274728d0d6f5c04e6a5a
Komrad <konradciurkiewicz@wp.pl>
2019-05-23 21:10:36 +0200

    Add generator logic

--- src/views/SketchGenerator.vue
+++ src/views/SketchGenerator.vue
@@ -16,11 +16,11 @@
               <div class="column is-half">
                 <p class="subtitle is-5">Level of details</p>
                 <circle-slider
-                  v-model="sliderValue"
+                  v-model="settings.gaussianBlur"
                   :side="150"
                   :min="0"
-                  :max="10000"
-                  :step-size="100"
+                  :max="10"
+                  :step-size="1"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
@@ -35,7 +35,7 @@
                   :side="150"
                   :min="0"
                   :max="10000"
-                  :step-size="100"
+                  :step-size="10"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
@@ -50,11 +50,11 @@
               <div class="column is-half">
                 <p class="subtitle is-5">Min treshold</p>
                 <circle-slider
-                  v-model="sliderValue"
+                  v-model="settings.lowThreshold"
                   :side="150"
                   :min="0"
-                  :max="10000"
-                  :step-size="100"
+                  :max="startSettings.maxTreshold"
+                  :step-size="5"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
@@ -65,11 +65,11 @@
               <div class="column is-half">
                 <p class="subtitle is-5">Max treshold</p>
                 <circle-slider
-                  v-model="sliderValue"
+                  v-model="settings.highThreshold"
                   :side="150"
                   :min="0"
-                  :max="10000"
-                  :step-size="100"
+                  :max="startSettings.highThreshold*3"
+                  :step-size="5"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
@@ -105,35 +105,67 @@ export default {
   data() {
     return {
       generetedSketch: {
-        src: ""
+        src: "",
+        grey: null,
       },
       inputImage: {
         url:
-          "https://pixabay.com/get/55e5dd444e52af14f6da8c7dda79347f153adde2574c704c70277fd29049c250_1280.jpg"
+          "https://pixabay.com/get/55e8d4454e53ac14f6da8c7dda79347f153adde2574c704c70277dd69e49c75d_1280.jpg"
+      },
+      startSettings: {
+          lowThreshold: 80,
+          highThreshold: 120,
+          gaussianBlur: 2,
+      },
+      settings:{
+          lowThreshold: 80,
+          highThreshold: 120,
+          gaussianBlur: 2,
       },
       sliderValue: 0
     };
   },
   methods: {
+    setDefaultSettings(img){
+      this.generetedSketch.grey = img.grey();
+      let treshold = this.generetedSketch.grey.getThreshold();
+
+      this.settings.lowThreshold = 0.7 * treshold;
+      this.settings.highThreshold = treshold;
+      this.settings.gaussianBlur = 1.1;
+      this.startSettings = this.settings;
+    },
     generateSketch() {
       Image.load(this.inputImage.url).then(img => {
-        const grey = img.grey();
-        let treshold = grey.getThreshold();
 
-        const edge = cannyEdgeDetector(grey, {
-          lowThreshold: 0.7 * treshold,
-          highThreshold: treshold,
-          gaussianBlur: 1.1
+        this.setDefaultSettings(img);
+
+        this.drawSketch();
+      });
+    },
+    drawSketch(){
+      const edge = cannyEdgeDetector(this.generetedSketch.grey, {
+          lowThreshold: this.settings.lowThreshold ,
+          highThreshold: this.settings.highThreshold,
+          gaussianBlur: this.settings.gaussianBlur,
         });
 
         var outImg = edge.invert();
         this.generetedSketch.src = outImg.toDataURL();
-      });
     }
   },
   created() {
     this.generateSketch();
+  },
+  watch: {
+      settings: {
+          handler: function(newValue) {
+            this.drawSketch();
+          },
+          deep: true
+      }
   }
+
 };
 </script>
 <style lang="scss">
