be38fb3009ef67794880c417d9952e2cba44b3cc
Komrad <konradciurkiewicz@wp.pl>
2019-05-23 21:10:36 +0200

    Add generator logic

---
+++ src/components/Loader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="loader"></div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+    .loader {
+        position: absolute;
+        width: 4em;
+        height: 4em;
+        left: calc( 50% - 4em );
+        top: calc( 50% - 4em );
+
+    }
+</style>
--- src/views/SketchGenerator.vue
+++ src/views/SketchGenerator.vue
@@ -1,41 +1,46 @@
 <template>
-  <div class="generator">
+  <div class="generator" @mouseup="stopChange()">
     <div class="container is-fluid">
       
       <div class="columns">
         <div class="column is-three-quarters">
-          <div class="box">
+          <div class="box hasLoader">
+            <Loader v-if="showLoader"></Loader>
             <img v-bind:src="generetedSketch.src">
           </div>
         </div>
         <div class="column is-one-quarter">
           <div class="box has-text-centered	">
           <p class="title ">Settings</p>
-
             <div class="columns">
-              <div class="column is-half">
+              <div class="column is-half" >
                 <p class="subtitle is-5">Level of details</p>
+                <div @mousedown = "startChange()" >
                 <circle-slider
-                  v-model="sliderValue"
+                  v-model="settings.gaussianBlur"
                   :side="150"
                   :min="0"
-                  :max="10000"
-                  :step-size="100"
+                  :max="10"
+                  :step-size="1"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
                   circle-color="#cecece"
                   progress-color="#F34213"
-                  knob-color="#F4542A"                ></circle-slider>
+                  knob-color="#F4542A"          
+                  
+                ></circle-slider>
+                </div>
               </div>
               <div class="column is-half">
                 <p class="subtitle is-5">Brightness</p>
+                <div @mousedown = "startChange()">
                 <circle-slider
                   v-model="sliderValue"
                   :side="150"
                   :min="0"
                   :max="10000"
-                  :step-size="100"
+                  :step-size="10"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
@@ -43,33 +48,37 @@
                   progress-color="#F34213"
                   knob-color="#F4542A"             
                 ></circle-slider>
+                </div>
               </div>
 
             </div>
             <div class="columns">
               <div class="column is-half">
                 <p class="subtitle is-5">Min treshold</p>
+                <div @mousedown = "startChange()">
                 <circle-slider
-                  v-model="sliderValue"
+                  v-model="settings.lowThreshold"
                   :side="150"
                   :min="0"
-                  :max="10000"
-                  :step-size="100"
+                  :max="startSettings.maxTreshold"
+                  :step-size="5"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
                   circle-color="#cecece"
                   progress-color="#F34213"
                   knob-color="#F4542A"                ></circle-slider>
+                </div>
               </div>
               <div class="column is-half">
                 <p class="subtitle is-5">Max treshold</p>
+                <div @mousedown = "startChange()">
                 <circle-slider
-                  v-model="sliderValue"
+                  v-model="settings.highThreshold"
                   :side="150"
                   :min="0"
-                  :max="10000"
-                  :step-size="100"
+                  :max="startSettings.highThreshold*3"
+                  :step-size="5"
                   :circle-width-rel="20"
                   :progress-width-rel="10"
                   :knob-radius="10"
@@ -77,6 +86,7 @@
                   progress-color="#F34213"
                   knob-color="#F4542A"             
                 ></circle-slider>
+                </div>
               </div>
 
             </div>
@@ -86,7 +96,7 @@
                     <span class="icon">
                       <font-awesome-icon icon="file-download" />   
                      </span>
-                    <span class="listElement_showMore">Download sketch</span>
+                    <span class="listElement_showMore" @click="downloadSketch()">Download sketch</span>
                   </a>
               </div>
             </div>
@@ -99,41 +109,84 @@
 <script>
 import axios from "axios";
 import cannyEdgeDetector from "canny-edge-detector";
+import downloadjs from "downloadjs";
 import Image from "image-js";
-
+import Loader from '@/components/Loader.vue'
 export default {
   data() {
     return {
       generetedSketch: {
-        src: ""
+        src: "",
+        grey: null,
       },
       inputImage: {
         url:
-          "https://pixabay.com/get/55e5dd444e52af14f6da8c7dda79347f153adde2574c704c70277fd29049c250_1280.jpg"
+          "https://pixabay.com/get/5ee1d042484fad0bffd8992cc429377b1638daf8525474417d2878d39045_1280.jpg"
       },
-      sliderValue: 0
+      startSettings: {
+          lowThreshold: 80,
+          highThreshold: 120,
+          gaussianBlur: 2,
+      },
+      settings:{
+          lowThreshold: 80,
+          highThreshold: 120,
+          gaussianBlur: 2,
+      },
+      sliderValue: 0,
+      showLoader: false,
+      timeLastChange: null,
+      
     };
   },
+  components: {
+      Loader
+  },
   methods: {
+    setDefaultSettings(img){
+      this.generetedSketch.grey = img.grey();
+      let treshold = this.generetedSketch.grey.getThreshold();
+
+      this.settings.lowThreshold = 0.7 * treshold;
+      this.settings.highThreshold = treshold;
+      this.settings.gaussianBlur = 1.1;
+      this.startSettings = this.settings;
+    },
     generateSketch() {
       Image.load(this.inputImage.url).then(img => {
-        const grey = img.grey();
-        let treshold = grey.getThreshold();
 
-        const edge = cannyEdgeDetector(grey, {
-          lowThreshold: 0.7 * treshold,
-          highThreshold: treshold,
-          gaussianBlur: 1.1
+        this.setDefaultSettings(img);
+
+        this.drawSketch();
+      });
+    },
+    startChange() {
+        this.showLoader = true;
+    },
+    stopChange(){
+      this.drawSketch();
+      this.showLoader = false;
+    },
+    drawSketch(){
+      const edge = cannyEdgeDetector(this.generetedSketch.grey, {
+          lowThreshold: this.settings.lowThreshold ,
+          highThreshold: this.settings.highThreshold,
+          gaussianBlur: this.settings.gaussianBlur,
         });
 
         var outImg = edge.invert();
         this.generetedSketch.src = outImg.toDataURL();
-      });
+
+    },
+    downloadSketch(){
+      downloadjs(this.generetedSketch.src, 'sketch.png', 'image/png');
     }
   },
   created() {
     this.generateSketch();
-  }
+  },
+
+
 };
 </script>
 <style lang="scss">
